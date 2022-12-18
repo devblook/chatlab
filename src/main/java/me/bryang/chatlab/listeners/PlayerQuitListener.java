00ad9492f3cf3861d8b136f7a37124b226c61546
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-12-18 15:37:44 -0500

    feat: create a quit listener

---
+++ src/main/java/me/bryang/chatlab/listeners/PlayerQuitListener.java
@@ -0,0 +1,35 @@
+package me.bryang.chatlab.listeners;
+
+import me.bryang.chatlab.ChatLab;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+public class PlayerQuitListener implements Listener {
+
+    @Inject
+    private ChatLab chatLab;
+
+    public void leaveEvent(PlayerQuitEvent event){
+
+        if (event.getPlayer().hasMetadata("ChatLab::sendMessage")){
+
+            Player sender = event.getPlayer();
+            Player target =  Bukkit.getPlayer(
+                    UUID.fromString(sender.getMetadata("ChatLab::privateMessage").get(0).asString()));
+
+            sender.removeMetadata("ChatLab::sendMessage", chatLab);
+
+            if (target.getMetadata("ChatLab::sendMessage").get(0).asString()
+                    .equalsIgnoreCase(sender.getUniqueId().toString())){
+
+                target.removeMetadata("ChatLab::sendMessage", chatLab);
+            }
+
+        }
+    }
+}
