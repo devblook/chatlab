7fd4463e47a9df2bf6ae65496bd937ad8c7ef086
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-12-18 14:43:53 -0500

    feat: create the file module

---
+++ src/main/java/me/bryang/chatlab/modules/FileModule.java
@@ -0,0 +1,24 @@
+package me.bryang.chatlab.modules;
+
+import me.bryang.chatlab.ChatLab;
+import me.bryang.chatlab.managers.FileManager;
+import team.unnamed.inject.Binder;
+import team.unnamed.inject.Module;
+
+import javax.inject.Inject;
+
+public class FileModule implements Module {
+
+    private final ChatLab plugin;
+
+    public FileModule(ChatLab plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(FileManager.class).named("config").toInstance(new FileManager(plugin, "config"));
+        binder.bind(FileManager.class).named("messages").toInstance(new FileManager(plugin, "messages"));
+
+    }
+}
