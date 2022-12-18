13f14d7d3b46faf68c87881d3787bd12817ce119
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-12-18 15:38:07 -0500

    register a quit listener

--- src/main/java/me/bryang/chatlab/loaders/ListenersLoader.java
+++ src/main/java/me/bryang/chatlab/loaders/ListenersLoader.java
@@ -1,13 +1,22 @@
 package me.bryang.chatlab.loaders;
 
+import me.bryang.chatlab.ChatLab;
 import me.bryang.chatlab.Loader;
+import me.bryang.chatlab.listeners.PlayerQuitListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 
+import javax.inject.Inject;
+
 public class ListenersLoader implements Loader {
+
+    @Inject
+    private ChatLab chatLab;
 
     @Override
     public void load() {
-        PluginManager pl = Bukkit.getPluginManager();
+        PluginManager pluginManager = Bukkit.getPluginManager();
+
+        pluginManager.registerEvents(new PlayerQuitListener(), chatLab);
     }
 }
