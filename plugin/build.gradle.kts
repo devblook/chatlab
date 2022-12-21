7588a197e4262d4683d64c1827e915b287302ba4
jonakls <asmot54@hotmail.com>
2022-12-20 19:37:25 -0500

    build(plugin): add custom jar name and relocations for unnamed/inject and fixed/command-flow libraries

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,4 +7,17 @@ dependencies{
     implementation("me.fixeddev:commandflow-bukkit:0.5.2")
     implementation("team.unnamed:inject:1.0.1")
     implementation(project(":api"))
+
+    testRuntimeOnly(project(":plugin"))
+    testRuntimeOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+}
+
+tasks {
+    shadowJar {
+        archiveFileName.set("ChatLab-${project.version}.jar")
+
+        relocate("me.fixeddev", "${project.group}.chatlab.libs.commandflow")
+        relocate("team.unnamed.inject", "${project.group}.chatlab.libs.inject")
+
+    }
 }
