c09147c47e4d24f74923d996421c1719d2e58dfb
jonakls <asmot54@hotmail.com>
2022-12-20 19:55:37 -0500

    test: add implementations classes of plugin for junit test

---
+++ plugin/src/test/java/me/bryang/chatlab/test/ChatLabTest.java
@@ -0,0 +1,15 @@
+package me.bryang.chatlab.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class ChatLabTest {
+
+    @Test
+    public void testUsers() {
+        UUID uuid = UUID.randomUUID();
+
+
+    }
+}
