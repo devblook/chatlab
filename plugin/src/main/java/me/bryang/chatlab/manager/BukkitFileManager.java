19cc6f4c192064ff389be728a1027bda9cd8ca0a
jonakls <asmot54@hotmail.com>
2022-12-20 19:53:44 -0500

    feat(config): change the way configuration files are obtained and created

---
+++ plugin/src/main/java/me/bryang/chatlab/manager/BukkitFileManager.java
@@ -0,0 +1,56 @@
+package me.bryang.chatlab.manager;
+
+import me.bryang.chatlab.ChatLab;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+
+public class BukkitFileManager {
+
+    private final File file;
+    private FileConfiguration config;
+
+    public BukkitFileManager(File folder, String fileName) {
+        if (!folder.exists() && !folder.mkdirs()) {
+            throw new IllegalStateException("Plugin folder" + folder.getName() + "cannot be created");
+        }
+        this.file = new File(folder, fileName + ".yml");
+
+        if (!file.exists()) {
+            try (InputStream stream = getClass().getClassLoader().getResourceAsStream(file.getName())) {
+                if (stream != null) {
+                    Files.copy(stream, file.toPath());
+                }
+            } catch (IOException e) {
+                throw new UncheckedIOException("An error occurred while loading file '" + fileName + "'.", e);
+            }
+        }
+        reload();
+    }
+
+    public BukkitFileManager(ChatLab plugin, String fileName) {
+        this(plugin.getDataFolder(), fileName);
+    }
+
+    public void reload() {
+        this.config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void save() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            throw new UncheckedIOException("An error occurred while saving file '" + file.getName() + "'.", e);
+        }
+    }
+
+    public FileConfiguration get() {
+        return config;
+    }
+
+}
