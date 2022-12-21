3b846346d6fb46d0a2f4d94e9fd7c6c0723c9fb0
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-12-21 16:26:04 -0500

    merge: accept merge

--- plugin/src/main/java/me/bryang/chatlab/command/CommandService.java
+++ plugin/src/main/java/me/bryang/chatlab/command/CommandService.java
@@ -1,16 +1,8 @@
 package me.bryang.chatlab.command;
 
 import me.bryang.chatlab.api.Service;
-<<<<<<< HEAD:plugin/src/main/java/me/bryang/chatlab/service/CommandService.java
-import me.bryang.chatlab.command.MainCommand;
-import me.bryang.chatlab.command.MsgCommand;
-import me.bryang.chatlab.command.ReplyCommand;
-import me.bryang.chatlab.service.translator.CommandTranslatorLoader;
-import me.bryang.chatlab.manager.FileManager;
-=======
 import me.bryang.chatlab.command.translator.CommandCustomTranslator;
 import me.bryang.chatlab.manager.BukkitFileManager;
->>>>>>> cfc6452564f5dfd3aa5aeccf60a2d74fd869cc22:plugin/src/main/java/me/bryang/chatlab/command/CommandService.java
 import me.fixeddev.commandflow.CommandManager;
 import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilder;
 import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilderImpl;
@@ -42,22 +34,9 @@ public class CommandService implements Service {
         partInjector.install(new DefaultsModule());
         partInjector.install(new BukkitModule());
 
-<<<<<<< HEAD:plugin/src/main/java/me/bryang/chatlab/service/CommandService.java
-        builder = new AnnotatedCommandTreeBuilderImpl(partInjector);
-        registerCommands(new MsgCommand(), new ReplyCommand(), new MainCommand());
-    }
-
-    public void registerCommands(CommandClass... commandClasses){
-
-        for (CommandClass commandClass : commandClasses){
-
-            commandManager.registerCommands(builder.fromClass(commandClass));
-
-=======
         AnnotatedCommandTreeBuilder builder = new AnnotatedCommandTreeBuilderImpl(partInjector);
         for (CommandClass command : commands) {
             commandManager.registerCommands(builder.fromClass(command));
->>>>>>> cfc6452564f5dfd3aa5aeccf60a2d74fd869cc22:plugin/src/main/java/me/bryang/chatlab/command/CommandService.java
         }
     }
 
