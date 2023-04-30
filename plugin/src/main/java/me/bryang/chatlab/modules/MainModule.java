eaabe2bc825db6be282abcd2419e7a2063488fec
BryanG <47282239+BryanGDv@users.noreply.github.com>
2023-04-30 14:44:06 -0500

    feat: change path method to a final variable

--- plugin/src/main/java/me/bryang/chatlab/modules/MainModule.java
+++ plugin/src/main/java/me/bryang/chatlab/modules/MainModule.java
@@ -13,15 +13,18 @@ import me.bryang.chatlab.user.User;
 import team.unnamed.inject.AbstractModule;
 import team.unnamed.inject.key.TypeReference;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainModule extends AbstractModule {
 
     private final ChatLab plugin;
+    private final Path pluginPath;
 
     public MainModule(ChatLab plugin){
         this.plugin = plugin;
+        this.pluginPath = plugin.getDataFolder().toPath();
     }
 
     @Override
@@ -35,16 +38,20 @@ public class MainModule extends AbstractModule {
 
         bind(CommandCustomTranslator.class)
                 .toInstance(new CommandCustomTranslator());
+
         bind(FileCreator.class)
-                .toInstance(new FileCreator(plugin.getDataFolder().toPath(), "config"));
+                .toInstance(new FileCreator(pluginPath, "config"));
+
         bind(FileCreator.class)
                 .named("messages")
-                .toInstance(new FileCreator(plugin.getDataFolder().toPath(), "messages"));
+                .toInstance(new FileCreator(pluginPath, "messages"));
 
         bind(new TypeReference<Map<String, User>>(){})
                 .toInstance(new HashMap<>());
 
-        bind(SenderManager.class).toInstance(new SenderManager(plugin));
+        bind(SenderManager.class)
+                .toInstance(new SenderManager(plugin));
+
         install(new ListenerModule());
         install(new CommandModule());
     }
