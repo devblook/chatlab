4808e7544cce96f8e7c1bbfd1af9ef57f0b639ea
BryanG <47282239+BryanGDv@users.noreply.github.com>
2023-05-01 15:36:23 -0500

    feat: add reply option en listener

--- plugin/src/main/java/me/bryang/chatlab/listeners/PlayerRegistryListener.java
+++ plugin/src/main/java/me/bryang/chatlab/listeners/PlayerRegistryListener.java
@@ -1,18 +1,30 @@
 package me.bryang.chatlab.listeners;
 
+import me.bryang.chatlab.FileCreator;
+import me.bryang.chatlab.manager.SenderManager;
 import me.bryang.chatlab.user.User;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import team.unnamed.inject.InjectAll;
 
 import javax.inject.Inject;
 import java.util.Map;
 
+@InjectAll
 public class PlayerRegistryListener implements Listener {
 
-    @Inject
     private Map<String, User> users;
+
+    private FileCreator configFile;
+    private SenderManager senderManager;
+
+
+
+
 
     @EventHandler
     public void onRegistry(PlayerJoinEvent event) {
@@ -21,6 +33,20 @@ public class PlayerRegistryListener implements Listener {
 
     @EventHandler
     public void onUnRegistry(PlayerQuitEvent event) {
+
+        User user = users.get(event.getPlayer().getUniqueId().toString());
+
+        if (!user.hasRecentMessenger()) {
+            return;
+        }
+
+        Player sender = Bukkit.getPlayer(user.recentMessenger());
+
+        senderManager.sendMessage(sender, configFile.getString("reply.left")
+                .replace("%target%", event.getPlayer().getName()));
+
+        user.recentMessenger(null);
+
 
     }
 }
