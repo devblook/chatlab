25647f707e392d56c7c9640b935d509afddfce05
BryanG <47282239+BryanGDv@users.noreply.github.com>
2023-05-01 15:35:00 -0500

    refactor: replace all injects to a only one annotation to inject all variables

--- plugin/src/main/java/me/bryang/chatlab/services/translator/CommandServices.java
+++ plugin/src/main/java/me/bryang/chatlab/services/translator/CommandServices.java
@@ -10,22 +10,21 @@ import me.fixeddev.commandflow.annotated.part.PartInjector;
 import me.fixeddev.commandflow.annotated.part.defaults.DefaultsModule;
 import me.fixeddev.commandflow.bukkit.BukkitCommandManager;
 import me.fixeddev.commandflow.bukkit.factory.BukkitModule;
+import team.unnamed.inject.InjectAll;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Set;
 
 
+@InjectAll
 public class CommandServices implements Service {
 
-    @Inject
     @Named("messages")
     private FileCreator messagesFile;
 
-    @Inject
     private Set<CommandClass> commands;
 
-    @Inject
     private CommandCustomTranslator commandCustomTranslator;
 
     @Override
