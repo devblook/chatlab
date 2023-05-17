19ef53d8464ee17f0abfee8546b98cb21fe5b5fc
jonakls <asmot54@hotmail.com>
2023-05-16 22:44:09 -0500

    build(relocations): changes in the methodology for relocation of units
    style: spaces that were left over in the code are cleaned up

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,27 +1,33 @@
+import org.gradle.configurationcache.extensions.capitalized
+
 plugins {
     id("chatlab.common-conventions")
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 dependencies {
-
     implementation("me.fixeddev:commandflow-bukkit:0.5.2")
     implementation("team.unnamed:inject:1.0.1")
     compileOnly("org.spongepowered:configurate-hocon:4.0.0")
-
 }
 
 tasks {
     shadowJar {
+        archiveBaseName.set(rootProject.name.capitalized())
 
-        archiveFileName.set("ChatLab-${project.version}.jar")
-
-        relocate("me.fixeddev", "${project.group}.chatlab.libs.commandflow")
-        relocate("team.unnamed.inject", "${project.group}.chatlab.libs.inject")
-
+        arrayOf(
+            "me.fixeddev",
+            "javax",
+            "org",
+            "team.unnamed.inject"
+        ).forEach {
+            relocate(it, "${project.group}.chatlab.libs.${it}")
+        }
     }
 
-
+    build {
+        dependsOn(shadowJar)
+    }
 }
 
 tasks.withType<JavaCompile> {
