fa82f1228571e47a7d493779e2529a2663f54647
BryanG <47282239+BryanGDv@users.noreply.github.com>
2023-06-03 12:21:17 -0500

    style: optimize imports

--- plugin/src/main/java/me/bryang/chatlab/listener/PlayerChatListener.java
+++ plugin/src/main/java/me/bryang/chatlab/listener/PlayerChatListener.java
@@ -1,22 +1,14 @@
 package me.bryang.chatlab.listener;
 
-import io.papermc.paper.chat.ChatRenderer;
-import io.papermc.paper.event.player.AsyncChatDecorateEvent;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.bryang.chatlab.configuration.ConfigurationContainer;
 import me.bryang.chatlab.configuration.section.RootSection;
 import me.bryang.chatlab.manager.MessageManager;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import net.kyori.text.serializer.plain.PlainComponentSerializer;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.slf4j.Logger;
 import team.unnamed.inject.InjectAll;
 
 
