6a8dc69718edab0e880a4f8d67a2d156821570d7
BryanG <47282239+BryanGDv@users.noreply.github.com>
2023-06-03 23:15:22 -0500

    feat(user): create ignored players list

--- plugin/src/main/java/me/bryang/chatlab/user/User.java
+++ plugin/src/main/java/me/bryang/chatlab/user/User.java
@@ -1,11 +1,14 @@
 package me.bryang.chatlab.user;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public class User {
 
     private UUID recentMessenger;
+    private Set<String> ignoredPlayers;
 
 
     @Nullable
@@ -21,5 +24,19 @@ public class User {
         return this.recentMessenger != null;
     }
 
+    public void putIgnorePlayers(Set<String> ignoredPlayers) {
+        this.ignoredPlayers = ignoredPlayers;
+    }
 
+    public void ignore(UUID target) {
+        ignoredPlayers.add(target.toString());
+    }
+
+    public void unIgnore(UUID target){
+        ignoredPlayers.remove(target.toString());
+    }
+
+    public boolean containsIgnoredPlayers(UUID target){
+        return ignoredPlayers.contains(target.toString());
+    }
 }
