534d60da0f44e121ec32f988477c6fc20e55c1bb
BryanG <47282239+BryanGDv@users.noreply.github.com>
2023-06-05 22:51:29 -0500

    feat(custom-translator): put new keys in the filter and set a new condition system

--- plugin/src/main/java/me/bryang/chatlab/service/command/CommandCustomTranslator.java
+++ plugin/src/main/java/me/bryang/chatlab/service/command/CommandCustomTranslator.java
@@ -4,6 +4,10 @@ import me.bryang.chatlab.configuration.ConfigurationContainer;
 import me.bryang.chatlab.configuration.section.MessageSection;
 import me.fixeddev.commandflow.Namespace;
 import me.fixeddev.commandflow.translator.TranslationProvider;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.ChatColor;
+import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -14,13 +18,28 @@ public class CommandCustomTranslator implements TranslationProvider {
     @Inject
     private ConfigurationContainer<MessageSection> messageContainer;
 
+    @Inject
+    private Logger logger;
+
+
     @Override
-    public String getTranslation(Namespace namespace, String key) {
+    public String getTranslation(Namespace namespace, String path){
         MessageSection messageSection = messageContainer.get();
 
-        if (key.equals("sender.only-player")) {
-            return messageSection.error.console;
-        }
-        return "If u see this message, please contact with us.";
+        String key = path.split("\\|")[0].trim();
+
+        String text = switch (key) {
+            case "sender.only_player" -> messageSection.error.console;
+            case "command.subcommand.invalid" -> messageSection.error.noArgument
+                    .replace("<usage>", path.split("\\|")[1]);
+            case "player.offline" -> messageSection.error.playerOffline
+                    .replace("<player>", "%s");
+            default -> "If u see this message, please contact with us.";
+        };
+
+
+        return ChatColor.translateAlternateColorCodes('&', LegacyComponentSerializer.legacyAmpersand().serialize(MiniMessage.miniMessage().deserialize(text)));
+
+
     }
 }
