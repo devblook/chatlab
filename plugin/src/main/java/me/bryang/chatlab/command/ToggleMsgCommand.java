5f9689fc68254b6b3440eb7a9566f5beaeabe2f9
BryanG <davidbryanrs@gmail.com>
2023-07-29 18:11:24 -0500

    feat(toggle-msg): add command to toggle private messages

---
+++ plugin/src/main/java/me/bryang/chatlab/command/ToggleMsgCommand.java
@@ -0,0 +1,41 @@
+package me.bryang.chatlab.command;
+
+import me.bryang.chatlab.configuration.ConfigurationContainer;
+import me.bryang.chatlab.configuration.section.MessageSection;
+import me.bryang.chatlab.configuration.section.RootSection;
+import me.bryang.chatlab.message.MessageManager;
+import me.bryang.chatlab.user.User;
+import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.annotation.Command;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import org.bukkit.entity.Player;
+import team.unnamed.inject.InjectAll;
+
+import java.util.Map;
+
+@InjectAll
+public class ToggleMsgCommand implements CommandClass {
+
+	private Map<String, User> userData;
+	private MessageManager messageManager;
+	private ConfigurationContainer<RootSection> configContainer;
+	private ConfigurationContainer<MessageSection> messageContainer;
+
+	@Command(
+		names = "toggle-msg",
+		desc = "Toggle private messages"
+	)
+	public void execute(@Sender Player sender) {
+
+		RootSection configFile = configContainer.get();
+		User user = userData.get(sender.getUniqueId().toString());
+
+		if (!user.privateMessages()) {
+			messageManager.sendMessage(sender, configFile.privateMessage.toggle.enable);
+		} else {
+			messageManager.sendMessage(sender, configFile.privateMessage.toggle.disable);
+		}
+
+		user.privateMessages(!user.privateMessages());
+	}
+}
--- plugin/src/main/java/me/bryang/chatlab/module/submodule/CommandModule.java
+++ plugin/src/main/java/me/bryang/chatlab/module/submodule/CommandModule.java
@@ -1,10 +1,6 @@
 package me.bryang.chatlab.module.submodule;
 
-import me.bryang.chatlab.command.IgnoreCommand;
-import me.bryang.chatlab.command.MainCommand;
-import me.bryang.chatlab.command.MessageCommand;
-import me.bryang.chatlab.command.ReplyCommand;
-import me.bryang.chatlab.command.UnIgnoreCommand;
+import me.bryang.chatlab.command.*;
 import me.fixeddev.commandflow.annotated.CommandClass;
 import team.unnamed.inject.AbstractModule;
 
@@ -12,9 +8,11 @@ public class CommandModule extends AbstractModule {
 
 	@Override
 	public void configure() {
-		multibind(CommandClass.class).asSet()
+		multibind(CommandClass.class)
+			.asSet()
 			.to(MainCommand.class)
 			.to(MessageCommand.class)
+			.to(ToggleMsgCommand.class)
 			.to(ReplyCommand.class)
 			.to(IgnoreCommand.class)
 			.to(UnIgnoreCommand.class);
