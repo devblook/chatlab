ad50897dc63e61207d1325db194c9a331f96cbe2
BryanG <davidbryanrs@gmail.com>
2023-07-29 18:23:39 -0500

    refactor(version): remove unnecesary variable for changed old system

--- plugin/src/main/java/me/bryang/chatlab/module/MainModule.java
+++ plugin/src/main/java/me/bryang/chatlab/module/MainModule.java
@@ -1,7 +1,10 @@
 package me.bryang.chatlab.module;
 
 import me.bryang.chatlab.ChatLab;
-import me.bryang.chatlab.module.submodule.*;
+import me.bryang.chatlab.module.submodule.CommandModule;
+import me.bryang.chatlab.module.submodule.ConfigurationModule;
+import me.bryang.chatlab.module.submodule.ListenerModule;
+import me.bryang.chatlab.module.submodule.ServiceModule;
 import me.bryang.chatlab.user.User;
 import org.slf4j.Logger;
 import team.unnamed.inject.AbstractModule;
@@ -13,15 +16,13 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("UnstableApiUsage")
 public class MainModule extends AbstractModule {
 
 	private final ChatLab plugin;
-	private final Path pluginPath;
 
 	public MainModule(ChatLab plugin) {
 		this.plugin = plugin;
-		this.pluginPath = plugin.getDataFolder().toPath();
+
 	}
 
 	@Provides
@@ -38,8 +39,10 @@ public class MainModule extends AbstractModule {
 
 		bind(Path.class)
 			.named("plugin-folder")
-			.toInstance(pluginPath);
-
+			.toInstance(plugin.getDataFolder().toPath());
+		bind(String.class)
+			.named("plugin-version")
+			.toInstance(plugin.getPluginMeta().getVersion());
 		bind(new TypeReference<Map<String, User>>() {
 		})
 			.toInstance(new HashMap<>());
@@ -49,4 +52,5 @@ public class MainModule extends AbstractModule {
 		install(new ServiceModule());
 		install(new ConfigurationModule());
 	}
+
 }
