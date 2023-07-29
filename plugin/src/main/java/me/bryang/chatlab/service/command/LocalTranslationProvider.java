65953c9ab820ca0d60ab2521fb511e3e7c77b77c
BryanG <davidbryanrs@gmail.com>
2023-07-29 18:08:27 -0500

    refactor(usage-builder): change translation provider name

--- plugin/src/main/java/me/bryang/chatlab/service/command/LocalTranslationProvider.java
+++ plugin/src/main/java/me/bryang/chatlab/service/command/LocalTranslationProvider.java
@@ -6,13 +6,12 @@ import me.fixeddev.commandflow.Namespace;
 import me.fixeddev.commandflow.translator.TranslationProvider;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.bukkit.ChatColor;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class CommandCustomTranslator implements TranslationProvider {
+public class LocalTranslationProvider implements TranslationProvider {
 
 	@Inject
 	private ConfigurationContainer<MessageSection> messageContainer;
@@ -21,22 +20,17 @@ public class CommandCustomTranslator implements TranslationProvider {
 	public String getTranslation(Namespace namespace, String path) {
 		MessageSection messageSection = messageContainer.get();
 
-		String key = path.split("\\|")[0];
-
-		String text = switch (key) {
-			case "sender.only_player" -> messageSection.error.console;
-			case "command.subcommand.invalid" -> messageSection.error.noArgument
-				.replace("<usage>", path.split("\\|")[1]);
+		String text = switch (path) {
+			case "sender.only-player" -> messageSection.error.console;
 			case "player.offline" -> messageSection.error.playerOffline
 				.replace("<player>", "%s");
 			case "command.no-permission" -> messageSection.error.noPermission;
 
-			default -> "If u see this message, please contact with us.";
+			default -> "If u see this message, please contact in the discord support. Key: " + path;
 		};
 
 
-		return ChatColor.translateAlternateColorCodes('&', LegacyComponentSerializer.legacyAmpersand()
-			.serialize(MiniMessage.miniMessage().deserialize(text)));
+		return LegacyComponentSerializer.legacySection().serialize(MiniMessage.miniMessage().deserialize(text));
 
 
 	}
