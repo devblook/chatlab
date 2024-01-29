a1b7dec389a979f1340ac5a34e410c846749fe0b
BryanG <davidbryanrs@gmail.com>
2024-01-29 12:43:56 -0500

    feat(vault): add VaultAPI as soft-dependency

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,6 +4,13 @@ import org.gradle.configurationcache.extensions.capitalized
 plugins {
     id("chatlab.common-conventions")
     id("com.github.johnrengelman.shadow") version "7.1.2"
+
+}
+
+repositories{
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.unnamed.team/repository/unnamed-public/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
@@ -11,6 +18,7 @@ dependencies {
     compileOnly("org.spongepowered:configurate-hocon:4.0.0")
     compileOnly("me.fixeddev:commandflow-bukkit:0.6.0")
     compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.1.0")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 }
 
 tasks {
