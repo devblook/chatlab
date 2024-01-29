e4c67013e9e940e61d04011dfd0f253b3beff1bc
BryanG <davidbryanrs@gmail.com>
2024-01-29 12:36:59 -0500

    fix(event-handler): make event-priority low to give compatibility

--- plugin/src/main/java/me/bryang/chatlab/listener/PlayerChatListener.java
+++ plugin/src/main/java/me/bryang/chatlab/listener/PlayerChatListener.java
@@ -1,11 +1,12 @@
 package me.bryang.chatlab.listener;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
-import me.bryang.chatlab.chat.ChatFormatter;
+import me.bryang.chatlab.chat.GroupFormatHandler;
 import me.bryang.chatlab.configuration.ConfigurationContainer;
 import me.bryang.chatlab.configuration.section.RootSection;
 import me.bryang.chatlab.message.MessageManager;
-import me.bryang.chatlab.user.User;
+import me.bryang.chatlab.storage.repository.Repository;
+import me.bryang.chatlab.storage.user.User;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
@@ -16,31 +17,32 @@ import org.bukkit.event.Listener;
 import org.slf4j.Logger;
 import team.unnamed.inject.InjectAll;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
-import java.util.Map;
-import java.util.Set;
+import java.util.List;
 
 @Singleton
 @InjectAll
 public class PlayerChatListener implements Listener {
 
+	@Named("users")
+	private Repository<User> userRepository;
+
 	private ConfigurationContainer<RootSection> configurationContainer;
 
-	private Map<String, User> userData;
-
 	private MessageManager messageManager;
-	private ChatFormatter chatFormatter;
+	private GroupFormatHandler groupFormatHandler;
 
 	private Logger logger;
 
-	@EventHandler(priority = EventPriority.LOWEST)
+	@EventHandler(priority = EventPriority.LOW)
 	public void onChat(AsyncChatEvent event) {
 
 		Player sender = event.getPlayer();
 		event.viewers().removeIf(audience -> {
 
 			if (audience instanceof Player player) {
-				Set<String> ignoredPlayers = userData.get(player.getUniqueId().toString()).ignoredPlayers();
+				List<String> ignoredPlayers = userRepository.findById(player.getUniqueId().toString()).ignoredPlayers();
 
 				if (sender.hasPermission("clab.ignore-bypass")) {
 					return false;
@@ -67,7 +69,8 @@ public class PlayerChatListener implements Listener {
 				? Placeholder.parsed("message", formattedMessage)
 				: Placeholder.unparsed("message", formattedMessage);
 
-			String format = chatFormatter.format(sender);
+			;
+			String format = groupFormatHandler.format(sender);
 
 			return messageManager.formatChat(source, format, messagePlaceholder);
 
