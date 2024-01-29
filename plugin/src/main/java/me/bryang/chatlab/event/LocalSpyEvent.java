8e01790cdcba1b4891d06a84d556ff6d98b28423
BryanG <davidbryanrs@gmail.com>
2024-01-29 12:27:43 -0500

    fix: fix bug when you parse the message variable and remove unnecessary process

--- plugin/src/main/java/me/bryang/chatlab/event/LocalSpyEvent.java
+++ plugin/src/main/java/me/bryang/chatlab/event/LocalSpyEvent.java
@@ -1,9 +1,10 @@
-package me.bryang.chatlab.socialspy;
+package me.bryang.chatlab.event;
 
 import me.bryang.chatlab.configuration.ConfigurationContainer;
 import me.bryang.chatlab.configuration.section.RootSection;
 import me.bryang.chatlab.message.MessageManager;
-import me.bryang.chatlab.user.User;
+import me.bryang.chatlab.storage.repository.Repository;
+import me.bryang.chatlab.storage.user.User;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
@@ -13,9 +14,7 @@ import team.unnamed.inject.InjectAll;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @InjectAll
 @Singleton
@@ -24,12 +23,12 @@ public class LocalSpyEvent {
 	@Named("users-in-spy")
 	private List<String> usersInSpy;
 	@Named("users")
-	private Map<String, User> users;
+	private Repository<User> userRepository;
 
 	private MessageManager messageManager;
 	private ConfigurationContainer<RootSection> configContainer;
 
-	public void call(Player sender, Player receptor, String message){
+	public void call(Player sender, Player receptor, String message) {
 
 		Component socialSpyMessage = messageManager.parse(configContainer.
 				get()
@@ -38,11 +37,11 @@ public class LocalSpyEvent {
 
 			Placeholder.unparsed("sender", sender.getName()),
 			Placeholder.unparsed("receptor", receptor.getName()),
-			Placeholder.unparsed("messages", message));
+			Placeholder.unparsed("message", message));
 
-		CompletableFuture.runAsync(() -> usersInSpy
+		usersInSpy
 			.stream()
 			.map(target -> Bukkit.getPlayer(UUID.fromString(target)))
-			.forEach(target -> sender.sendMessage(socialSpyMessage)));
+			.forEach(target -> target.sendMessage(socialSpyMessage));
 	}
 }
